54ffef843daf67a1dd737e80b681968b9c766075
Uight <46501936+Uight@users.noreply.github.com>
2019-01-09 20:23:56 +0100

    Update BiomesMapping.java
    Removed unused Variables and added a new string array with all possible Biomes

--- src/com/gmail/junehanabi/modifybiomes/BiomesMapping.java
+++ src/com/gmail/junehanabi/modifybiomes/BiomesMapping.java
@@ -26,79 +26,12 @@ import net.minecraft.server.v1_13_R2.BiomeBase;
  * @author junebug
  */
 public class BiomesMapping {
-    public static final String OCEAN = "OCEAN";
-    public static final String PLAINS = "PLAINS";
-    public static final String DESERT = "DESERT";
-    public static final String MOUNTAINS = "MOUNTAINS";
-    public static final String FOREST = "FOREST";
-    public static final String TAIGA = "TAIGA";
-    public static final String SWAMP = "SWAMP";
-    public static final String RIVER = "RIVER";
-    public static final String NETHER = "NETHER";
-    public static final String THE_END = "THE_END";
-    public static final String FROZEN_OCEAN = "FROZEN_OCEAN";
-    public static final String FROZEN_RIVER = "FROZEN_RIVER";
-    public static final String SNOWY_TUNDRA = "SNOWY_TUNDRA";
-    public static final String SNOWY_MOUNTAINS = "SNOWY_MOUNTAINS";
-    public static final String MUSHROOM_FIELDS = "MUSHROOM_FIELDS";
-    public static final String MUSHROOM_FIELD_SHORE = "MUSHROOM_FIELD_SHORE";
-    public static final String BEACH = "BEACH";
-    public static final String DESERT_HILLS = "DESERT_HILLS";
-    public static final String WOODED_HILLS = "WOODED_HILLS";
-    public static final String TAIGA_HILLS = "TAIGA_HILLS";
-    public static final String MOUNTAIN_EDGE = "MOUNTAIN_EDGE";
-    public static final String JUNGLE = "JUNGLE";
-    public static final String JUNGLE_HILLS = "JUNGLE_HILLS";
-    public static final String JUNGLE_EDGE = "JUNGLE_EDGE";
-    public static final String DEEP_OCEAN = "DEEP_OCEAN";
-    public static final String STONE_SHORE = "STONE_SHORE";
-    public static final String SNOWY_BEACH = "SNOWY_BEACH";
-    public static final String BIRCH_FOREST = "BIRCH_FOREST";
-    public static final String BIRCH_FOREST_HILLS = "BIRCH_FOREST_HILLS";
-    public static final String DARK_FOREST = "DARK_FOREST";
-    public static final String SNOWY_TAIGA = "SNOWY_TAIGA";
-    public static final String SNOWY_TAIGA_HILLS = "SNOWY_TAIGA_HILLS";
-    public static final String GIANT_TREE_TAIGA = "GIANT_TREE_TAIGA";
-    public static final String GIANT_TREE_TAIGA_HILLS = "GIANT_TREE_TAIGA_HILLS";
-    public static final String WOODED_MOUNTAINS = "WOODED_MOUNTAINS";
-    public static final String SAVANNA = "SAVANNA";
-    public static final String SAVANNA_PLATEAU = "SAVANNA_PLATEAU";
-    public static final String BADLANDS = "BADLANDS";
-    public static final String WOODED_BADLANDS_PLATEAU = "WOODED_BADLANDS_PLATEAU";
-    public static final String BADLANDS_PLATEAU = "BADLANDS_PLATEAU";
-    public static final String SMALL_END_ISLANDS = "SMALL_END_ISLANDS";
-    public static final String END_MIDLANDS = "END_MIDLANDS";
-    public static final String END_HIGHLANDS = "END_HIGHLANDS";
-    public static final String END_BARRENS = "END_BARRENS";
-    public static final String WARM_OCEAN = "WARM_OCEAN";
-    public static final String LUKEWARM_OCEAN = "LUKEWARM_OCEAN";
-    public static final String COLD_OCEAN = "COLD_OCEAN";
-    public static final String DEEP_WARM_OCEAN = "DEEP_WARM_OCEAN";
-    public static final String DEEP_LUKEWARM_OCEAN = "DEEP_LUKEWARM_OCEAN";
-    public static final String DEEP_COLD_OCEAN = "DEEP_COLD_OCEAN";
-    public static final String DEEP_FROZEN_OCEAN = "DEEP_FROZEN_OCEAN";
-    public static final String THE_VOID = "THE_VOID";
-    public static final String SUNFLOWER_PLAINS = "SUNFLOWER_PLAINS";
-    public static final String DESERT_LAKES = "DESERT_LAKES";
-    public static final String GRAVELLY_MOUNTAINS = "GRAVELLY_MOUNTAINS";
-    public static final String FLOWER_FOREST = "FLOWER_FOREST";
-    public static final String TAIGA_MOUNTAINS = "TAIGA_MOUNTAINS";
-    public static final String SWAMP_HILLS = "SWAMP_HILLS";
-    public static final String ICE_SPIKES = "ICE_SPIKES";
-    public static final String MODIFIED_JUNGLE = "MODIFIED_JUNGLE";
-    public static final String MODIFIED_JUNGLE_EDGE = "MODIFIED_JUNGLE_EDGE";
-    public static final String TALL_BIRCH_FOREST = "TALL_BIRCH_FOREST";
-    public static final String TALL_BIRCH_HILLS = "TALL_BIRCH_HILLS";
-    public static final String DARK_FOREST_HILLS = "DARK_FOREST_HILLS";
-    public static final String SNOWY_TAIGA_MOUNTAINS = "SNOWY_TAIGA_MOUNTAINS";
-    public static final String GIANT_SPRUCE_TAIGA = "GIANT_SPRUCE_TAIGA";
-    public static final String GIANT_SPRUCE_TAIGA_HILLS = "GIANT_SPRUCE_TAIGA_HILLS";
-    public static final String MODIFIED_GRAVELLY_MOUNTAINS = "MODIFIED_GRAVELLY_MOUNTAINS";
-    public static final String SHATTERED_SAVANNA = "SHATTERED_SAVANNA";
-    public static final String SHATTERED_SAVANNA_PLATEAU = "SHATTERED_SAVANNA_PLATEAU";
-    public static final String ERODED_BADLANDS = "ERODED_BADLANDS";
-    public static final String MODIFIED_WOODED_BADLANDS_PLATEAU = "MODIFIED_WOODED_BADLANDS_PLATEAU";
-    public static final String MODIFIED_BADLANDS_PLATEAU = "MODIFIED_BADLANDS_PLATEAU";
+    public static final String[] AllowedBiomes = {"OCEAN","PLAINS","DESERT","MOUNTAINS","FOREST","TAIGA","SWAMP","RIVER","NETHER","THE_END", "FROZEN_OCEAN", "FROZEN_RIVER", "SNOWY_TUNDRA", "SNOWY_MOUNTAINS", "MUSHROOM_FIELDS", "MUSHROOM_FIELD_SHORE", "BEACH", "DESERT_HILLS",
+                                                "WOODED_HILLS", "TAIGA_HILLS", "MOUNTAIN_EDGE", "JUNGLE", "JUNGLE_HILLS", "JUNGLE_EDGE", "DEEP_OCEAN", "STONE_SHORE", "SNOWY_BEACH", "BIRCH_FOREST", "BIRCH_FOREST_HILLS", "DARK_FOREST", "SNOWY_TAIGA", "SNOWY_TAIGA_HILLS",
+                                                "GIANT_TREE_TAIGA", "GIANT_TREE_TAIGA_HILLS", "WOODED_MOUNTAINS", "SAVANNA", "SAVANNA_PLATEAU", "BADLANDS", "WOODED_BADLANDS_PLATEAU", "BADLANDS_PLATEAU", "SMALL_END_ISLANDS", "END_MIDLANDS", "END_HIGHLANDS", "END_BARRENS",
+                                                "WARM_OCEAN", "LUKEWARM_OCEAN", "COLD_OCEAN", "DEEP_WARM_OCEAN", "DEEP_LUKEWARM_OCEAN", "DEEP_COLD_OCEAN", "DEEP_FROZEN_OCEAN", "THE_VOID", "SUNFLOWER_PLAINS", "DESERT_LAKES", "GRAVELLY_MOUNTAINS", "FLOWER_FOREST", "TAIGA_MOUNTAINS",
+                                                "SWAMP_HILLS", "ICE_SPIKES", "MODIFIED_JUNGLE", "MODIFIED_JUNGLE_EDGE", "TALL_BIRCH_FOREST", "TALL_BIRCH_HILLS", "DARK_FOREST_HILLS", "SNOWY_TAIGA_MOUNTAINS", "GIANT_SPRUCE_TAIGA", "GIANT_SPRUCE_TAIGA_HILLS", "MODIFIED_GRAVELLY_MOUNTAINS",
+                                                "SHATTERED_SAVANNA", "SHATTERED_SAVANNA_PLATEAU", "ERODED_BADLANDS", "MODIFIED_WOODED_BADLANDS_PLATEAU", "MODIFIED_BADLANDS_PLATEAU"};
     
     // Converts ID above to its associated BiomeBase
     public static BiomeBase GetBiome(String ID) 
